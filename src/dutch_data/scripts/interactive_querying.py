f6c7a2ef0f11e1b8b1cb1e616f10d5a3a9d70112
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2023-12-10 20:42:34 +0100

    add system prompt option

--- src/dutch_data/scripts/interactive_querying.py
+++ src/dutch_data/scripts/interactive_querying.py
@@ -23,6 +23,7 @@ def build_message(role: str, content: str) -> dict[str, str]:
 @app.command()
 def huggingface(
     model_name: Annotated[str, typer.Argument(help="the model name to use from HuggingFace's model hub (e.g. 'BramVanroy/Llama-2-13b-chat-dutch')")],
+    system_prompt: Annotated[Optional[str], typer.Option("-p", "--system-prompt", help="an optional system message")] = None,
     device_map: Annotated[Optional[str], typer.Option(help="device (map) to use. You probably want to set this to 'auto' or to a specific device")] = None,
     load_in_8bit: Annotated[bool, typer.Option(help="whether to load the model in 8 bit precision")] = False,
     load_in_4bit: Annotated[bool, typer.Option(help="whether to load the model in 4 bit precision")] = False,
@@ -36,7 +37,7 @@ def huggingface(
         load_in_8bit=load_in_8bit,
         load_in_4bit=load_in_4bit,
     )
-    interactive_playground(generator)
+    interactive_playground(generator, system_prompt=system_prompt)
 
 
 @app.command()
@@ -45,6 +46,7 @@ def azure(
     credentials_profile: Annotated[
         str, Argument(help="which credential profile (key) to use from the credentials file")
     ],
+    system_prompt: Annotated[Optional[str], typer.Option("-p", "--system-prompt", help="an optional system message")] = None,
 ):
     """
     Interactive playground for querying the Azure API with given credentials and profile
@@ -52,16 +54,17 @@ def azure(
     credentials = Credentials.from_json(credentials_file, credentials_profile)
     querier = AzureQuerier.from_credentials(credentials)
     generator = AzureTextGenerator(querier)
-    interactive_playground(generator)
+    interactive_playground(generator, system_prompt=system_prompt)
 
 
 def interactive_playground(
     generator: TextGenerator,
+    system_prompt: str | None = None,
 ):
     """
     Interactive playground for querying the Azure API or HuggingFace models
     """
-    messages = []
+    messages = [] if not system_prompt else [build_message("system", system_prompt)]
     print("Enter a message/reply. Enter 'exit' to quit. Enter 'restart' to clear message history.")
     while user_query := input(f"{Back.CYAN}{Fore.WHITE}User:{Style.RESET_ALL} "):
         if user_query == "exit":
